01450a16393351e686c71c261be1f2d73bbe756c
Patrick Woodworth <p77wood@gmail.com>
2021-12-04 19:27:21 -0600

    Day2 Part2 completed.

--- day2/dive.cpp
+++ day2/dive.cpp
@@ -8,21 +8,58 @@
 
 using namespace std;
 
+void rawPart();
+void aimPart();
+
 int main(int argc, char const *argv[]){
+    rawPart();
+    aimPart();
+}
+
+void rawPart(){
     int depth = 0;
     int h_pos = 0;
     int distance;
-
-    string temp;
-    string direction;
-
-    vector<string> directionVector;
-    vector<int> distanceVector;
-
     ifstream inputFile;
 
     inputFile.open("inputFile.txt");
-    while(getline(inputFile, temp)){
-        //TODO: split temp into direction and distance using space as delimiter
+    string direction;
+    while(inputFile >> direction >> distance){
+        // inputFile >> direction >> distance;
+        // cout << direction << " " << distance << endl;
+        if(direction == "forward"){
+            h_pos += distance;
+        }else if(direction == "up"){
+            depth -= distance;
+        }else if(direction == "down"){
+            depth += distance;
+        }
+        distance = 0;
     }
+    cout << "Part 1 Horizontal Position * Depth = " << h_pos * depth << endl;
+}
+
+void aimPart(){
+    int depth = 0;
+    int h_pos = 0;
+    int aim = 0;
+    int distance = 0;
+    ifstream inputFile;
+
+    inputFile.open("inputFile.txt");
+    string direction;
+    while(inputFile >> direction >> distance){
+        // inputFile >> direction >> distance;
+        // cout << direction << " " << distance << endl;
+        if(direction == "forward"){
+            h_pos += distance;
+            depth += (aim*distance);
+        }else if(direction == "up"){
+            aim -= distance;
+        }else if(direction == "down"){
+            aim += distance;
+        }
+        distance = 0;
+    }
+    cout << "Part 2 Horizontal Position * Depth = " << h_pos * depth << endl;
 }
